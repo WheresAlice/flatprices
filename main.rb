a365e073a3eeb02596fca9a24be8b60f7f350589
kaerast <alice@kaerast.info>
2011-05-28 14:52:40 +0100

    added ttl to output page

--- main.rb
+++ main.rb
@@ -52,11 +52,11 @@ end
 
 get '/' do
   haml :index, :locals => { :Cities => {
-    :Bradford => "&pound;#{sprintf("%.2f", City.new('Bradford').flatprice}",
-    :Leeds => "&pound#{sprintf("%.2f", City.new('Leeds').flatprice}",
-    :Manchester => "&pound#{sprintf("%.2f", City.new('Manchester').flatprice}",
-    :Newcastle => "&pound;#{sprintf("%.2f", City.new('Newcastle').flatprice}"
-  }}
+    :Bradford => "&pound;#{sprintf("%.2f", City.new('Bradford').flatprice)}",
+    :Leeds => "&pound#{sprintf("%.2f", City.new('Leeds').flatprice)}",
+    :Manchester => "&pound#{sprintf("%.2f", City.new('Manchester').flatprice)}",
+    :Newcastle => "&pound;#{sprintf("%.2f", City.new('Newcastle').flatprice)}"
+  }, :ttl => REDIS.ttl('bradford')}
 end
 
 get '/ping' do
