3808e84a04c0a1286ed4870ce3be0aef66b034e1
kaerast <alice@kaerast.info>
2011-05-28 14:23:46 +0100

    caching in Redis

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 gem "sinatra"
 gem "hpricot"
 gem "haml"
+gem "redis"
--- main.rb
+++ main.rb
@@ -5,6 +5,12 @@ require 'haml'
 
 set :haml, :format => :html5
 
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
 class Array
   def sum
     inject(0.0) { |result, el| result + el }
@@ -27,16 +33,24 @@ class City
   end
   
   def flatprice
-    prices = []
-    doc = open("http://www.gumtree.com/flats-and-houses-for-rent-offered/#{name.downcase}") { |f| Hpricot(f) }
-    doc.search("//div[@class='price']").each do |price|
-      prices.push( price.inner_text[/[0-9]+\.?[0-9]+/].to_i )
-    end
-    prices.mean
+    ttl = REDIS.ttl(name.downcase)
+    if (ttl < 0) {
+      prices = []
+      doc = open("http://www.gumtree.com/flats-and-houses-for-rent-offered/#{name.downcase}") { |f| Hpricot(f) }
+      doc.search("//div[@class='price']").each do |price|
+        prices.push( price.inner_text[/[0-9]+\.?[0-9]+/].to_i )
+      end
+      REDIS.set name.downcase, prices.mean
+      REDIS.expire name.downcase, 600
+      @prices = prices.mean
+    } else {
+      @prices = REDIS.get(name.downcase)
+    }
+    @prices
   end
 end
 
 get '/' do
-  response['Cache-Control'] = 'public, max-age=600'
+  headers['Cache-Control'] = 'public, max-age=600'
   haml :index, :locals => { :Cities => {:Bradford => "&pound;#{City.new('Bradford').flatprice}", :Leeds => "&pound#{City.new('Leeds').flatprice}", :Manchester => "&pound#{City.new('Manchester').flatprice}", :Newcastle => "&pound;#{City.new('Newcastle').flatprice}"}}
 end
